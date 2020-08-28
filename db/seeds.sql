8cd962f962a2e6c0f7a70d38ad73b724f9a4b1eb
brian <brianra2nil@hotmail.com>
2020-08-27 17:18:11 -0700

    update

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE bergers_db;
+
+INSERT INTO burgers (name, devoured)
+VALUES ('cheeseburger', false),
+('baconburger', false),
+('avocadoburger', false);
---
+++ models/burger.js
@@ -0,0 +1,19 @@
+const orm = require('../config/orm.js')
+
+const model = { }
+
+model.getAll = (cb) => {
+  orm.read('burgers', burgers => cb(burgers))
+}
+
+model.createOne = (burger, cb) => {
+  orm.create('burgers', burger, id => cb(id))
+}
+
+model.updateOne = (updates, where, cb) => {
+  orm.update('burgers', updates, where, () => cb())
+}
+
+
+
+module.exports = model
